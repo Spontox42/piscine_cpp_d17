31737d76cb2049e2eadf909d4e464dc2dc0ef5cd
petit_x <petit_x@epitech.eu>
2016-01-22 11:08:59 +0100

    01

---
+++ ex00/find.hpp
@@ -0,0 +1,25 @@
+/*
+** find.hpp for find/home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d17/ex00/find.hpp
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Fri Jan 22 10:59:44 2016 petit_x - Marlon Petit
+** Last update Fri Jan 22 10:59:46 2016 Marlon Petit
+*/
+
+#ifndef FIND_HPP_
+# define FIND_HPP_
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+template<class T>
+
+class T::iterator do_find(T& t, int i)
+{
+    return std::find(t.begin(), t.end(), i);
+}
+
+#endif /* !FIND_HPP_ */
